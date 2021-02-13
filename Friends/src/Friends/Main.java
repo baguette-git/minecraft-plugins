c7b140aaa65858f4febbfb756b39536ca67132c3
yair sharon <yair2663@gmail.com>
2021-02-13 16:33:45 +0200

    add Friends project, empty plugin

---
+++ Friends/src/Friends/Main.java
@@ -0,0 +1,12 @@
+package Friends;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        getLogger().info("Friends plugin is now active");
+    }
+}
